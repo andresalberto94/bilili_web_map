285c2944998a1c3b0a8065bc099b1bbcbb367e94
Jorge Daniel García Girón <54127703+jordan10793@users.noreply.github.com>
2022-09-14 17:55:14 -0600

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -4,13 +4,6 @@ var google=L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {at
 
 }).addTo(map);
 
-//document.getElementById('Área de Interés').addEventListener('change', function(e){
-//    let coords = e.target.value.split(",");
-//    map.flyTo(coords,13);
-//})
-
-var carto_light = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'})
-
 var minimap = new L.Control.MiniMap(carto_light,
     {
         toggleDisplay: true,
@@ -18,30 +11,6 @@ var minimap = new L.Control.MiniMap(carto_light,
         position: "bottomright"
 
     }).addTo(map);
-
-//limite centroamerica
-
-function style3(feature){
-    return {
-        weight: 2,
-        opacity: 7,
-        color: 'white',
-        dashArray: '1',
-        fillOpacity: 0
-    };
-}
-var ca= L.geoJson(ca,{style: style3}).addTo(map);
-
-//
-function style4(feature){
-    return {
-        weight: 2,
-        opacity: 7,
-        color: 'red',
-        dashArray: '1',
-        fillOpacity: 0.1
-    };
-}
 
 // area de conservacion
 
@@ -106,9 +75,6 @@ function style2(feature){
         fillOpacity: '0.7'
     };
 }
-//var area_conservacion= L.geoJson(area_conservacion,{onEachFeature: onEachFeature_2,style: style2}).bindPopup(function(layers){
-//    return layers.feature.properties.nombre_ac}, {"className": "a_c"}).addTo(map);
-
 
 var area_conservacion= L.geoJson(area_conservacion,{onEachFeature: on_feature,style: style2}).bindPopup(function(layer){
     let div = L.DomUtil.create('div');
@@ -132,100 +98,6 @@ var area_conservacion= L.geoJson(area_conservacion,{onEachFeature: on_feature,st
     }
     ,{"className": "a_c"}).addTo(map);
 
-
-// funciones para resaltado
-
-function highlightFeature(e){
-    var layer=e.target;
-    layer.setStyle({
-        weight: 2,
-        color: '#e5f5f9',
-        dashArray: '5',
-        fillOpacity: 0.1
-    });
-    info.update(layer.feature.properties);
-}
-
-var co1;
-
-function resetHighlight_1(e){
-    co1.resetStyle(e.target);
-    info.update();
-}
-
-function zoomToFeature_1(e){
-    map.fitBounds(e.target.getBounds());
-}
-
-function onEachFeature_1(feature, layer){
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout : resetHighlight_1,
-        click:  zoomToFeature_1
-    })
-};
-
-function style1(feature){
-    return {
-        weight: 2,
-        opacity: 2,
-        color: '#3182bd',
-        dashArray: '1',
-        fillOpacity: 0.1
-    };
-}
-var co1= L.geoJson(co1,{style: style1, onEachFeature: onEachFeature_1 }).bindPopup(function(layers){
-    return layers.feature.properties.nombre_cb}, {"className": "co1"}).addTo(map);
-
-function style5(feature){
-    return {
-        weight: 2,
-        opacity: 2,
-        color: 'green',
-        dashArray: '1',
-        fillOpacity: 0.1
-    };
-}
-var m_s= L.geoJson(m_s,{style: style2}).bindPopup(function(layers){
-    return layers.feature.properties.name
-}).addTo(map);
-
-//rios 
-
-function style7(feature){
-    return {
-        weight: 2,
-        opacity: 2,
-        color: '#9ecae1',
-        dashArray: '1',
-        fillOpacity: 0.1
-    };
-}
-var rios= L.geoJson(rios,{style: style7}).bindPopup(function(layers){
-    return layers.feature.properties.Nombre}, {"className" : "z_p"}
-).addTo(map);
-
-
-// puntos de medicion del GAM
-
-function compostaje_gj_pointToLayer( feature , latlng) {
-    var opts = {};
-
-    const iconOptions = {"extraClasses": "fa-rotate-0", "icon": "eye-open", "iconColor": "white", "markerColor": "green", "prefix": "m_g"}
-    const iconRootAlias = L.AwesomeMarkers
-    opts.icon = new iconRootAlias.Icon(iconOptions)
-
-    return new L.Marker(latlng, opts)
-}
-function compostaje_gj_onEachFeature(feature, layer) {
-    layer.on({
-    });
-};
-
-var m_g= L.geoJson(m_g,{onEachFeature: compostaje_gj_onEachFeature, pointToLayer: compostaje_gj_pointToLayer}).bindPopup(function(layers){
-    return layers.feature.properties.Nombre},{"className": "m_g"}).addTo(map);
-
-
 // agregando control de capas y leyenda
 
 var baseLayers = {
@@ -233,24 +105,9 @@ var baseLayers = {
  };
  
  var overlays = {
-    "Límite de Centroamérica": ca,
     "Áreas de Conservación": area_conservacion,
-    "Áreas Silvestres":a_s,
-    "Corredores Biológicos": co1,
-    "Puntos de medición": m_s,
-    "GAM":m_g,
-    "Zona Protectora Maria Aguilar": zp_ma,
-    "Red Hídrica": rios,
  };
     
  L.control.layers(baseLayers, overlays, { collapsed:false, position:'bottomleft' }).addTo(map);
  area_conservacion.bringToFront();
- zp_ma.remove();
- a_s.remove();
- m_s.remove();
- m_g.remove();
- co1.remove();
- rios.remove();
-
-
  
